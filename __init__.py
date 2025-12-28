1c635fe1a329ceca01568f905e2b4a04e5ee3124
DarriEy <dae5@hi.is>
2025-12-28 09:06:20 -0600

    Added test scripts for building, point, watershed and regional models

--- __init__.py
+++ __init__.py
@@ -7,22 +7,29 @@ A comprehensive hydrological modeling platform for watershed analysis.
 Note: This package was previously known as CONFLUENCE. For backward compatibility,
 the old name is still supported but deprecated.
 """
-from symfluence_version import __version__
+try:
+    from symfluence_version import __version__
+except ImportError:
+    __version__ = "0.5.0"
 
 __author__ = "Darri Eythorsson"
 __email__ = "darri.eythorsson@ucalgary.ca"
 
-# Main API imports for convenience
-from utils.project.project_manager import ProjectManager
-from utils.project.workflow_orchestrator import WorkflowOrchestrator
-from utils.cli.cli_argument_manager import CLIArgumentManager
+# Main API imports for convenience (optional for testing)
+try:
+    from utils.project.project_manager import ProjectManager
+    from utils.project.workflow_orchestrator import WorkflowOrchestrator
+    from utils.cli.cli_argument_manager import CLIArgumentManager
 
-__all__ = [
-    "__version__",
-    "ProjectManager", 
-    "WorkflowOrchestrator", 
-    "CLIArgumentManager"
-]
+    __all__ = [
+        "__version__",
+        "ProjectManager",
+        "WorkflowOrchestrator",
+        "CLIArgumentManager"
+    ]
+except ImportError:
+    # If utils can't be imported (e.g., during testing), just export version
+    __all__ = ["__version__"]
 
 # Backward compatibility warning
 import warnings
